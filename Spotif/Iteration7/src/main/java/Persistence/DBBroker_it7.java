da87e9926e70a7754cde0e26b15002eca5838105
BekaBekeri <BekaBekeri@users.noreply.github.com>
2017-12-01 12:16:55 +0100

    Iteration 7

---
+++ Spotif/Iteration7/src/main/java/Persistence/DBBroker_it7.java
@@ -0,0 +1,5 @@
+package Persistence;
+
+public class DBBroker_it7 {
+
+}
---
+++ Spotif/Iteration7/src/main/java/Presentation/AppAdmin_it7.java
@@ -0,0 +1,43 @@
+package Presentation;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class AppAdmin_it7 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AppAdmin_it7 window = new AppAdmin_it7();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AppAdmin_it7() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
