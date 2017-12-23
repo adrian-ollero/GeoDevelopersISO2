dd46573e5e9cdc5bacd30381cabdd404fa0cc95d
adrian-ollero <adrian.ollero@alu.uclm.es>
2017-12-23 18:20:39 +0100

    Testing It1
    Testing Reports iteration 1

--- Spotif/Iteration8/src/main/java/Domain/DaoUser_it8.java
+++
@@ -1,8 +0,0 @@
-package Domain;
-
-public class DaoUser_it8{
-
-	public void sendMessage(Message message) {
-		
-	}
-}
--- Spotif/Iteration8/src/main/java/Persistence/DBBroker_it8.java
+++
@@ -1,8 +0,0 @@
-package Persistence;
-
-public class DBBroker_it8 extends DBBroker_it7{
-
-	public void sendMessage() {
-		
-	}
-}
